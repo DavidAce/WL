a834d18c617f26218103cb28a0905b453b84c1ba
DavidAce <kameboy@gmail.com>
2016-09-13 10:05:59 +0200

    create folder better

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+option=$1
 echo "Starting job"
 
 if [[ "$HOSTNAME" == *"triolith"* ]]
 then
     echo "We're on triolith!";
-    sbatch run_triolith.sh
+    sbatch run_triolith.sh ${option}
 else
     echo "We're on my pc!"
-    ./run_my_pc.sh
+    ./run_my_pc.sh ${option}
 fi
