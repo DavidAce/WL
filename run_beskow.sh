3427e9f80330c39a87714a1ade04fcc9115337e9
DavidAce <kameboy@gmail.com>
2016-10-07 17:02:15 +0200

    making cmakelists compatible with pdc

--- run_beskow.sh
+++ run_beskow.sh
@@ -2,8 +2,8 @@
 
 #SBATCH -J WL
 #SBATCH -A 2016-10-17
-#SBATCH -t 0-6:00:00
-#SBATCH -N 6
+#SBATCH -t 0-0:01:00
+#SBATCH -N 2
 #SBATCH --exclusive
 export OMP_NUM_THREADS=1
 aprun ./build/Release/WL
