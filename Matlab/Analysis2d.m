5c4e97f83b3681ef57145bb070493e7363e7cfa5
DavidAce <kameboy@gmail.com>
2016-10-10 10:38:23 +0200

    making cmakelists compatible with pdc

--- Matlab/Analysis2d.m
+++ Matlab/Analysis2d.m
@@ -1,18 +1,20 @@
 close all
 clear all
-L = 6;
+L = 16;
 N = L^2;
-j = 0;
-for i = 0:7
+j = 1;
+for i = 0:1
     dos = load(['../outdata/' num2str(j) '/dos' num2str(i) '.dat']);
     E   = load(['../outdata/' num2str(j) '/E'   num2str(i) '.dat']);
     M = load(  ['../outdata/' num2str(j) '/M' num2str(i) '.dat']);
     [c,u,T, dosE] = thermo2d(dos,E,M,N);
     figure(1);
     subplot(1,2,1);
+
+    dos(dos == 0) = nan;
+    dos = dos - min(min(dos));
     h = mesh(M,E, dos); 
     hold on;
-    
  	set(h, 'zdata', dos);
 	ylabel('E');
 	xlabel('M');
