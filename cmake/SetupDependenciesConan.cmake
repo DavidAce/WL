cfead3f4d261068b1f7371ee5e9a08b53f2faf28
DavidAce <aceituno@kth.se>
2022-09-16 13:26:14 +0200

    Transmit compiler flags to dependencies on conan builds

--- cmake/SetupDependenciesConan.cmake
+++ cmake/SetupDependenciesConan.cmake
@@ -10,30 +10,47 @@ if(WL_PACKAGE_MANAGER MATCHES "conan")
     # Download cmake-conan integrator
     if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan/conan.cmake")
         message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-        file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/release/0.17/conan.cmake"
-                "${CMAKE_BINARY_DIR}/conan/conan.cmake"
-                EXPECTED_HASH MD5=52a255a933397fdce3d0937f9c737e98
-                TLS_VERIFY ON)
+        file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
+             "${CMAKE_BINARY_DIR}/conan/conan.cmake"
+             EXPECTED_HASH MD5=81d5eab13a49f43527e35a90bfac6960
+             TLS_VERIFY ON)
     endif()
 
-    include(${CMAKE_BINARY_DIR}/conan/conan.cmake)
+    if(BUILD_SHARED_LIBS)
+        list(APPEND CONAN_OPTIONS OPTIONS "*:shared=True")
+    else()
+        list(APPEND CONAN_OPTIONS OPTIONS "*:shared=False")
+    endif()
+
+    if(CMAKE_BUILD_TYPE MATCHES "Debug")
+        list(APPEND CONAN_OPTIONS OPTIONS "ceres-solver:use_glog=False")
+    endif()
+
+    # Copy the current compiler flags to conan
+    string(TOUPPER "${CMAKE_BUILD_TYPE}" CONAN_BUILD_TYPE)
+    set(CONAN_CXXFLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_${CONAN_BUILD_TYPE}}")
+    set(CONAN_CFLAGS "${CMAKE_C_FLAGS} ${CMAKE_C_FLAGS_${CONAN_BUILD_TYPE}}")
+    set(CONAN_LDFLAGS "${CMAKE_EXE_LINKER_FLAGS}")
     conan_cmake_autodetect(CONAN_AUTODETECT)
     conan_cmake_install(
             CONAN_COMMAND ${CONAN_COMMAND}
-            BUILD missing outdated cascade
+            BUILD ${CONAN_BUILD} missing outdated cascade
+            UPDATE
             GENERATOR CMakeDeps
             SETTINGS ${CONAN_AUTODETECT}
             INSTALL_FOLDER ${CMAKE_BINARY_DIR}/conan
+            ENV CC=${CMAKE_C_COMPILER} # Fixes issue with CMake not detecting the right compiler when not building from scratch
+            ENV CXX=${CMAKE_CXX_COMPILER} # Fixes issue with CMake not detecting the right compiler when not building from scratch
+            ENV CXXFLAGS=${CONAN_CXXFLAGS}
+            ENV CFLAGS=${CONAN_CFLAGS}
+            ENV LDFLAGS=${CONAN_LDFLAGS}
+            ${CONAN_OPTIONS}
             PATH_OR_REFERENCE ${CMAKE_SOURCE_DIR}
     )
-
 
     ##################################################################
     ### Find all the things!                                       ###
     ##################################################################
-    if(NOT CONAN_CMAKE_SILENT_OUTPUT)
-        set(CONAN_CMAKE_SILENT_OUTPUT OFF) # Default is off
-    endif()
     list(PREPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR}/conan)
     list(PREPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR}/conan)
     list(REMOVE_DUPLICATES CMAKE_MODULE_PATH)
